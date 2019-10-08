82f19cee95c68665a5585ccc49de0063d37a9505
leo <leofallensoul@gmail.com>
2019-10-08 14:54:42 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -122,6 +122,94 @@ var getPosts = function (arr) {
     fragment.appendChild(element);
   }
 
+  // показ и закарытие, создание коментария и размещение его в разметке, для большой картинки
+
+  var previewImg = fragment.querySelectorAll('.picture');
+
+  var bigImageOpen = function (index) {
+    bigImg.src = 'photos/' + index + '.jpg';
+    bigPictureLikesCount.textContent = userPosts[index - 1].likes;
+    bigPictureImgDescription.textContent = userPosts[index].description;
+    bigPicture.classList.remove('hidden');
+  };
+
+  var onPrewievClick = function (element, index) {
+    element.addEventListener('click', function () {
+      bigImageOpen(index);
+    });
+  };
+
+  var onPrewievEnterKeydown = function (element, index) {
+    element.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === ENTER_BUTTON) {
+        bigImageOpen(index);
+      }
+    });
+  };
+
+  var getComment = function (globIndex, localIndex) {
+    var commentElement = document.createElement('li');
+    var commentElementImg = document.createElement('img');
+    var commentElementText = document.createElement('p');
+
+    commentElement.className = 'social__comment';
+    commentElementImg.className = 'social__picture';
+    commentElementText.className = 'social__text';
+
+    commentElementImg.src = userPosts[globIndex].comments[localIndex].avatar;
+    commentElementImg.alt = userPosts[globIndex].comments[localIndex].name;
+    commentElementText.textContent = userPosts[globIndex].comments[localIndex].message;
+
+    commentElement.appendChild(commentElementImg);
+    commentElement.appendChild(commentElementText);
+
+    return commentElement;
+  };
+
+  socialCommentCount.classList.add('visually-hidden');
+  commentLoader.classList.add('visually-hidden');
+
+  var onBigPictureButtonCloseClick = function (evt) {
+    evt.preventDefault();
+    bigPicture.classList.add('hidden');
+    bigPictureCommentsList.innerHTML = '';
+  };
+
+  var onBigpictureEscPress = function (evt) {
+    if (evt.keyCode === ESC_BUTTON) {
+      evt.preventDefault();
+      bigPicture.classList.add('hidden');
+      bigPictureCommentsList.innerHTML = '';
+    }
+  };
+
+  bigPictureClose.addEventListener('click', onBigPictureButtonCloseClick);
+  document.addEventListener('keydown', onBigpictureEscPress);
+
+  var getBigPictureComment = function (element, arr, globIndex) {
+    element.addEventListener('click', function () {
+      for (var ii = 0; ii < arr.comments.length; ii++) {
+        bigPictureCommentsList.appendChild(getComment(globIndex, ii));
+      }
+    });
+  };
+
+  for (var iii = 0; iii < previewImg.length; iii++) {
+    onPrewievClick(previewImg[iii], iii + 1);
+    onPrewievEnterKeydown(previewImg[iii], iii + 1);
+    getBigPictureComment(previewImg[iii], userPosts[iii], iii);
+  }
+
+  commentLoaderInput.addEventListener('focusin', function () {
+    bigPictureClose.removeEventListener('click', onBigPictureButtonCloseClick);
+    document.removeEventListener('keydown', onBigpictureEscPress);
+  });
+
+  commentLoaderInput.addEventListener('focusout', function () {
+    bigPictureClose.addEventListener('click', onBigPictureButtonCloseClick);
+    document.addEventListener('keydown', onBigpictureEscPress);
+  });
+
   return fragment;
 };
 
@@ -360,92 +448,4 @@ var getHashtagError = function () {
 hashtagInput.addEventListener('input', function () {
   var message = getHashtagError();
   hashtagInput.setCustomValidity(message);
-});
-
-var prewievImg = picturesList.querySelectorAll('.picture');
-
-var bigImageOpen = function (index) {
-  bigImg.src = 'photos/' + index + '.jpg';
-  bigPictureLikesCount.textContent = userPosts[index - 1].likes;
-  bigPictureImgDescription.textContent = userPosts[0].description;
-  bigPicture.classList.remove('hidden');
-};
-
-var onPrewievClick = function (element, index) {
-  element.addEventListener('click', function () {
-    bigImageOpen(index);
-  });
-};
-
-var onPrewievKeydown = function (element, index) {
-  element.addEventListener('keydown', function (evt) {
-    if (evt.keyCode === ENTER_BUTTON) {
-      bigImageOpen(index);
-    }
-  });
-};
-
-// показ и закарытие, создание коментария и размещение его в разметке, для большой картинки
-
-var getComment = function (globIndex, localIndex) {
-  var commentElement = document.createElement('li');
-  var commentElementImg = document.createElement('img');
-  var commentElementText = document.createElement('p');
-
-  commentElement.className = 'social__comment';
-  commentElementImg.className = 'social__picture';
-  commentElementText.className = 'social__text';
-
-  commentElementImg.src = userPosts[globIndex].comments[localIndex].avatar;
-  commentElementImg.alt = userPosts[globIndex].comments[localIndex].name;
-  commentElementText.textContent = userPosts[globIndex].comments[localIndex].message;
-
-  commentElement.appendChild(commentElementImg);
-  commentElement.appendChild(commentElementText);
-
-  return commentElement;
-};
-
-socialCommentCount.classList.add('visually-hidden');
-commentLoader.classList.add('visually-hidden');
-
-var onBigPictureButtonCloseClick = function (evt) {
-  evt.preventDefault();
-  bigPicture.classList.add('hidden');
-  bigPictureCommentsList.innerHTML = '';
-};
-
-var onBigpictureEscPress = function (evt) {
-  if (evt.keyCode === ESC_BUTTON) {
-    evt.preventDefault();
-    bigPicture.classList.add('hidden');
-    bigPictureCommentsList.innerHTML = '';
-  }
-};
-
-bigPictureClose.addEventListener('click', onBigPictureButtonCloseClick);
-document.addEventListener('keydown', onBigpictureEscPress);
-
-var getBigPictureComment = function (element, arr, globIndex) {
-  element.addEventListener('click', function () {
-    for (var ii = 0; ii < arr.comments.length; ii++) {
-      bigPictureCommentsList.appendChild(getComment(globIndex, ii));
-    }
-  });
-};
-
-for (var iii = 0; iii < prewievImg.length; iii++) {
-  onPrewievClick(prewievImg[iii], iii + 1);
-  onPrewievKeydown(prewievImg[iii], iii + 1);
-  getBigPictureComment(prewievImg[iii], userPosts[iii], iii);
-}
-
-commentLoaderInput.addEventListener('focusin', function () {
-  bigPictureClose.removeEventListener('click', onBigPictureButtonCloseClick);
-  document.removeEventListener('keydown', onBigpictureEscPress);
-});
-
-commentLoaderInput.addEventListener('focusout', function () {
-  bigPictureClose.addEventListener('click', onBigPictureButtonCloseClick);
-  document.addEventListener('keydown', onBigpictureEscPress);
 });
